b06909626a849ea4d49a5e01052eeb0e82694d33
Albiery Gonçalves dos Santos <albierygoncalves2@gmail.com>
2024-09-17 15:37:38 -0300

    #

--- src/utils/error.js
+++ src/utils/error.js
@@ -8,7 +8,7 @@ const loginCadastro = async error => {
         return 'Email ou senha incorretos'
     } else if (name.includes('to be unique')) {
         return 'Email já cadastrado'
-    } else if (name.includes('password deve ter no mínimo 6 caracterees')) {
+    } else if (name.includes('password deve ter no mínimo 6 caracteres')) {
         return 'A senha deve ter pelo menos 6 caracteres'
     }
     return null
