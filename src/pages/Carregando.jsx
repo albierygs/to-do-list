bf38247893c2eda2fb01c8cc85db75fba4cc3574
Albiery Gonçalves dos Santos <albierygoncalves2@gmail.com>
2024-11-12 17:22:20 -0300

    alteração efeito loading

--- src/pages/Carregando.jsx
+++ src/pages/Carregando.jsx
@@ -2,9 +2,7 @@ import style from '../styles/carregando.module.css'
 
 const Carregando = () => {
   return (
-    <div className={style.container}>
-      <div className={style.loader}></div>
-    </div>
+    <div className={style.loader}></div>
   )
 }
 
